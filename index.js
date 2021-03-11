9976e1ba11641a77c91f3fbc726aa3cd2e229f93
Mario Sanz <72298127+mario-sanz@users.noreply.github.com>
2021-03-11 11:36:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  VerlyRange('range-slider', '#0000ff');
+  VerlyRange('range-slider2', '#0000ff');
+  VerlyRange('range-slider3', '#0000ff');
+  VerlyRange('range-slider4', '#0000ff');
+
+}
